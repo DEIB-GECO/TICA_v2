d2704a17c077a9c196e47ae4b9d9129cc3fbbcde
Erlaad <stefano.perna@polimi.it>
2017-10-24 11:31:42 +0200

    Second interation of pipeline steps. All functions are written down but need to be tested. This is going to be a long mess.

--- tester/pipeline_steps.py
+++ tester/pipeline_steps.py
@@ -9,20 +9,79 @@ TF which they contain.
 Author: Stefano Perna (stefano.perna@polimi.it)
 Date: 18 October 2017
 Version: 0.1"""
+from tester.models import *
+
 import sys
-#import utils
 import gmql as pygmql
 import copy
-import tqdm as timer
+import numpy as np
+import pandas as pd
+import intervaltree as it
 
+# Chromosome names
+CHRS = list(map(lambda x: "chr" + str(x), range(1, 23))) + ["chrX", "chrY"]
 
 # Common functions
-def data_uploader(file_list):
-    """Loads datasets from the disk returns formatted GMQLDatasets.
-    Keyword arguments:
-        -- file_list: list of files to be accessed on disk
+def __format_checker__(file):
+    """Ancillary function of data_uploader. Returns a string
+    based on the format inspected in the file:
+    - 'narrowPeak': the file suits the ENCODE narrowPeak
+     specifications
+     - 'BED2': the file contains chromosome and start information
+     only,
+     - 'BED3': the file contains chromosome, start and stop
+     information,
+     - 'BED4': the file contains chromosome, start, stop and strand
+     information
+     - 'unknown_format': anything else.
     """
-    return GMQLDatasets
+    with open(file, 'r') as infile:
+        raw_data = list(infile.readlines)
+    # default separator is assumed to be \t
+    separator = '\t'
+    # comment is #
+    comment_search = [item[0] == '#' for item in raw_data]
+    if all(comment_search):
+        return 'unknown_format'
+    else:
+        true_indexes = [comment_search[i] == False
+                        for i in range(len(comment_search))]
+        if ';' in comment_search[true_indexes[0]] and \
+                        separator not in comment_search[true_indexes[0]]:
+            separator=';'
+        elif ',' in comment_search[true_indexes[0]] \
+                and separator not in comment_search[true_indexes[0]]:
+            sep=','
+        all_data = pd.read_csv(file,sep=separator, comment='#')
+        if True:
+            return 'narrowPeak'
+            #list of narrowpeak specifications
+        elif all([item in CHRS for item in all_data[0]]) \
+                and type(all_data[1]) == int \
+                and type(all_data[2]) == int \
+                and type(all_data[3]) == str:
+            return 'BED4'
+        elif all([item in CHRS for item in all_data[0]]) \
+                and type(all_data[1]) == int \
+                and type(all_data[2]) == int:
+            return 'BED3'
+        elif all([item in CHRS for item in all_data[0]]) \
+                and type(all_data[1]) == int:
+            return 'BED2'
+        else:
+            return 'unknown_format'
+    
+    
+def data_uploader(folder_list=None):
+    """Loads datasets from the disk and returns formatted GMQLDatasets.
+    Keyword arguments:
+        -- file_list: list of folders contaiining files to be
+        accessed on disk (default: None)
+    """
+    assert folder_list, "ERROR: folder list is empty. Aborting."
+    for place in folder_list:
+    
+    return
 
 
 def tfbs_filter(datasets, tf_list, window_size=1000, min_acc_value=3):
@@ -36,29 +95,27 @@ def tfbs_filter(datasets, tf_list, window_size=1000, min_acc_value=3):
         to be found in the moving window (default: 3)
     """
     # Assume tf_list is sorted
+    # Assume that input is loaded
     
-    ds_list = []
-
-    for index in range(len(tf_list)):
-        ds_list.append(datasets[index].to_GMQLDataset())
+    # ds_list = []
+    #
+    # for index in range(len(tf_list)):
+    #     ds_list.append(datasets[index].to_GMQLDataset())
     
     # First process the simple ones in batch
-    tfbs_data = ds_list[0]
-    for df in ds_list[1:]:
+    tfbs_data = datasets[0]
+    for df in datasets[1:]:
         tfbs_data = tfbs_data.union(df)
-    
-    print(tfbs_data.materialize().meta)
-    exit()
-    # NOTE: we assume to be already in 1bp length form after upload
-    # npeaks = tfbs_data.reg_project(
-    #     new_field_dict={
-    #         'start': hepg2_narrow_data.start + hepg2_narrow_data.peak,
-    #         'stop': hepg2_narrow_data.start
-    #                 + hepg2_narrow_data.peak + 1}
-    # )
+
+    npeaks = tfbs_data.reg_project(
+        new_field_dict={
+            'start': tfbs_data.start + tfbs_data.peak,
+            'stop': tfbs_data.start
+                    + tfbs_data.peak + 1}
+    )
 
     # NOTE: groupBy must be given in list format
-    covered = tfbs_data.cover(1, 'ANY', groupBy=['experiment_target'])
+    covered = npeaks.cover(1, 'ANY', groupBy=['experiment_target'])
 
     signals = covered
     # Enlarge by moving window
@@ -82,7 +139,8 @@ def tfbs_filter(datasets, tf_list, window_size=1000, min_acc_value=3):
     return filtered_signals
 
 
-def tfbs2tss_mapper(datasets, tss_dataset, target_folder='path/to/default'):
+def tfbs2tss_mapper(tfbs_file_list, target_list,
+                    tss_file_path, target_folder='path/to/default'):
     """Maps TFBSes to promoters of TSSes using IDs. Stores maps as
     comma-separated-values on disk and returns them as dicts (?).
     Keyword arguments:
@@ -91,22 +149,54 @@ def tfbs2tss_mapper(datasets, tss_dataset, target_folder='path/to/default'):
         -- target_folder: folder where TFBS_to_TSS maps will be stored
         (default: 'path/to/default')
     """
-    return dict
+    # Assume that input is loaded
+    # Assume that tfbs file list is sorted according to target_list
+    # Parse and load configuration variables
 
+    # Load TSS information from file
+    tsses_raw = pd.read_csv(tss_file_path, sep='\t', header=None).values
+    tsses = dict(
+        [
+            (c, [item for item in tsses_raw if item[1] == c])
+            for c in CHRS
+        ]
+    )
+    
+    to_execute = zip(target_list,tfbs_file_list)
+    
+    for item in to_execute:
+        tfbses_raw = pd.read_csv(item[1],
+                                 sep='\t', header=None).values
+        tfbses = dict(
+            [
+                (c, [item for item in tfbses_raw if item[1] == c])
+                for c in CHRS
+            ]
+        )
+        associated_tss = []
+        for c in CHRS:
+            f_tfbses = tfbses[c]
+            f_tss = it.IntervalTree.from_tuples(
+                [(item[2] - 2000, item[3] + 201, item[0])
+                 if item[6] == '+'
+                 else (item[2] - 200, item[3] + 2001, item[0])
+                 for item in tsses[c]]
+            )
+            c_map_ = [(tfbs[0], f_tss[tfbs[2]]) for tfbs in f_tfbses]
+            associated_tss += c_map_
+        with open('%s/%s_associated_tss.txt' %
+                          (target_folder, item[0]), 'w') \
+                as associated_outfile:
+            associated_outfile.writelines(
+                ['%d;%s\n' %
+                 (item[0], ','.join([str(tss_id)
+                                     for tss_id in sorted(
+                         [data[2] for data in list(item[1])])])
+                 if list(item[1]) else '-1')
+                 for item in associated_tss])
+        print('Map complete.')
+    return True
 
-def compute_mindistances(ds1, ds2, use_tsses=True):  # +a list of parameters
-    """Computes the mindistance couple distance distributions given
-    two TFBS datasets and returns a null table Model row for
-    insertion.
-    
-    Keyword arguments:
-        -- ds1, ds2: GMQLDatasets containing binding sites for each TF
-        and maps to TSS
-        -- use_tsses: defines whether the algorithm should search for
-        colocation in promoters. Use False for null distributions.
-        (default: True)"""
-    
-    return # A model for the appropriate table
 
 
 # Algorithmic workflow-only
@@ -138,7 +228,7 @@ def tss_filter(tss_file_path, cell_name, list_of_targets,
 
 
     # for testing purposes
-    #OUTFILE_PATH = 'test_2/'
+    OUTFILE_PATH = 'test_2/'
 
 
     # Data loading
@@ -183,7 +273,34 @@ def tss_filter(tss_file_path, cell_name, list_of_targets,
         & (enc_broad_full['biosample_term_name'] == cell_name)
         & (enc_broad_full['experiment_target'] == 'H3K4me1-human')
         ]
+    
+    # print('# Extract narrowpeaks.')
+    hepg2_narrow_df_list = []
+    for tf in list_of_targets:
+        hepg2_narrow_df_list.append(
+            enc_narrow_full[(enc_narrow_full[
+                                 'biosample_term_name'] == cell_name)
+                            & (enc_narrow_full['assay'] == 'ChIP-seq')
+                            & (
+                                enc_narrow_full['experiment_target'] == tf)])
+    hepg2_narrow_data = hepg2_narrow_df_list[0]
+    print(hepg2_narrow_data.materialise().regs)
+    for df in hepg2_narrow_df_list[1:]:
+        hepg2_narrow_data = hepg2_narrow_data.union(df)
 
+    # hepg2_narrow_data = enc_narrow_full[
+    #     (enc_narrow_full['biosample_term_name'] == CELL_NAME)
+    #     & (enc_narrow_full['assay'] == 'ChIP-seq')
+    # ]
+    npeaks = hepg2_narrow_data.reg_project(
+        new_field_dict={
+            'start': hepg2_narrow_data.start + hepg2_narrow_data.peak,
+            'stop': hepg2_narrow_data.start + hepg2_narrow_data.peak + 1}
+    )
+
+    merged_npeaks = npeaks.merge()
+    print(len(merged_npeaks.materialize().regs))
+    exit()
     strands = ['+', '-']  # To streamline the code
 
     results = []
@@ -206,7 +323,6 @@ def tss_filter(tss_file_path, cell_name, list_of_targets,
         f_exons = exons_on_hms.reg_select(
             exons_on_hms.count_exons_hm_4_exons != 0
         )
-
         # print('# Second extensions: to promoter size.')
         if this_strand == '+':
             promoters = f_exons.reg_project(
@@ -254,31 +370,6 @@ def tss_filter(tss_file_path, cell_name, list_of_targets,
                                         + promoter_length}
             )
 
-        # print('# Extract narrowpeaks.')
-        hepg2_narrow_df_list = []
-        for tf in list_of_targets:
-            hepg2_narrow_df_list.append(
-                enc_narrow_full[(enc_narrow_full[
-                                     'biosample_term_name'] == cell_name)
-                                & (enc_narrow_full['assay'] == 'ChIP-seq')
-                                & (
-                                enc_narrow_full['experiment_target'] == tf)])
-        hepg2_narrow_data = hepg2_narrow_df_list[0]
-    
-        for df in hepg2_narrow_df_list[1:]:
-            hepg2_narrow_data = hepg2_narrow_data.union(df)
-    
-        # hepg2_narrow_data = enc_narrow_full[
-        #     (enc_narrow_full['biosample_term_name'] == CELL_NAME)
-        #     & (enc_narrow_full['assay'] == 'ChIP-seq')
-        # ]
-        npeaks = hepg2_narrow_data.reg_project(
-            new_field_dict={
-                'start': hepg2_narrow_data.start + hepg2_narrow_data.peak,
-                'stop': hepg2_narrow_data.start + hepg2_narrow_data.peak + 1}
-        )
-    
-        merged_npeaks = npeaks.merge()
 
         # print('# Map promoters on narrowpeaks.')
         mapped_promoters = f_new_promoters.map(merged_npeaks,
@@ -288,7 +379,7 @@ def tss_filter(tss_file_path, cell_name, list_of_targets,
         f_mapped_promoters = mapped_promoters.reg_select(
             mapped_promoters.count_f_new_promoters_merged_peaks >= min_tfbs_count
         )
-    
+        print(len(f_mapped_promoters.materialize().regs))
         # Go back to 1bp size
         if this_strand == '+':
             mapped_tsses = f_mapped_promoters.reg_project(
@@ -315,11 +406,137 @@ def tss_filter(tss_file_path, cell_name, list_of_targets,
     return both_strands_res
 
 
+def compute_nulls(i_cell):  # +a list of parameters
+    """Computes the mindistance couple distance distributions given
+    two TFBS datasets and returns a null table Model row for
+    insertion.
+
+    Keyword arguments:
+        -- ds1, ds2: GMQLDatasets containing binding sites for each TF
+        and maps to TSS
+        -- use_tsses: defines whether the algorithm should search for
+        colocation in promoters. Use False for null distributions.
+        (default: True)"""
+    # TODO make this list reading from database
+    # SP: I'm adding two input parameters, cell and a tf_list. For now
+    # I don't use them but we should use them in the above TODO
+    include_list = ['ARID3A', 'ATF4', 'BHLHE40', 'BRCA1', 'CEBPB', 'CEBPD',
+                    'CHD2', 'CREM', 'CTCF', 'DNMT3B', 'DROSHA', 'ELF1',
+                    'EP300', 'ETV4', 'EZH2', 'FOSL2', 'FOXA1', 'FOXA2',
+                    'FOXK2', 'GABPA', 'GATA4', 'GTF2F1', 'HCFC1', 'HDAC2',
+                    'HHEX', 'HLF', 'HNF1A', 'HNF4A', 'HNF4G', 'HNRNPLL',
+                    'HSF1', 'IKZF1', 'IRF3', 'JUND', 'JUN', 'KAT2B', 'MAFF',
+                    'MAX', 'MAZ', 'MBD4', 'MNT', 'MXI1', 'MYBL2', 'MYC',
+                    'NFE2L2', 'NFIC', 'NR2C2', 'NR2F6', 'NRF1', 'PLRG1',
+                    'POLR2A', 'POLR2AphosphoS2', 'POLR2AphosphoS5',
+                    'RAD21', 'RAD51', 'RCOR1', 'REST', 'RFX5', 'RNF2',
+                    'RXRA', 'SIN3A', 'SIN3B', 'SMC3', 'SOX13', 'SP1',
+                    'SREBF1', 'SUZ12', 'TAF1', 'TBP', 'TCF7L2', 'TCF7',
+                    'TEAD4', 'TFAP4', 'USF1', 'USF2', 'YBX1', 'YY1',
+                    'ZBTB40', 'ZBTB7A', 'ZHX2', 'ZKSCAN1', 'ZMYM3', 'ZNF143',
+                    'ZNF207', 'ZNF274', 'ZNF384']
+    nullpd = pd.DataFrame(list(Hepg2Null.objects.filter(
+        tf1__in=include_list).filter(tf2__in=include_list).exclude(
+        average__isnull=True).values()))
+
+    max_distances = sorted(nullpd.reset_index()['max_distance'].unique())
+
+    bootstrap_size = 10000
+    nullpds = {}
+    percs = [1, 5, 10, 20]
+    for max_distance in max_distances:
+        temp = nullpd[nullpd['max_distance'] == max_distance].drop(
+            ['tf1', 'tf2', 'max_distance'], axis=1
+        )
+        temp = temp.sample(bootstrap_size, replace=True)
+        x = []
+        names = []
+        for col in temp.columns:
+            temp1 = (temp[col])
+            tempavgs = [np.percentile(temp1, q=perc) for perc in percs]
+            x = x + tempavgs
+
+            nms = [col + "-" + str(perc) for perc in percs]
+            names = names + nms
+        nullpds[max_distance] = x
+    logger.warning('calculated_null is ready')
+
+    result = pd.DataFrame(nullpds, index=names)
+    # return result # OR POSSIBLY .to_dict
+    # THAT GENERATED TWO LEVEL OF DICTIONARY, MAXD, AND
+    # THEN NAME THAT GENERATED BEFORE
+    return result.to_dict()  # THAT GENERATED TWO LEVEL OF DICTIONARY,
+    # MAXD, AND THEN NAME THAT GENERATED BEFORE
+
 # User-guided workflow-only
-def output_results(Model):
+def __get_tf_list__(cell):
+    """Ancillary function to check_tfs. Returns the list of all
+    available tfs in a cell line. If cell is "user", instead returns
+    the list of TFs provided by the user in the current session."""
+    # We need to optimize this
+    return CellLineTfs.objects.filter(cell_line=cell).values_list(
+        'tf', flat=True)
+
+def output_results(candidate, input_cell, max_distance=2200,
+                   tail_size=1000,min_tss=0.01,
+                   min_count=1,p_value=0.2,
+                   min_num_true_test=3,
+                   test_list=('average', 'mad', 'median', 'tail_1000')):
     """Checks whether a candidate passes the TICA workflow tests
     and returns result and distance distribution graph.
     Keyword arguments:
         -- Model: table row containing statistics for the candidate
     """
-    return GMQLDatasets
+    # lazy val
+    all_tfs = sorted(list(__get_tf_list__()))
+    calculated_null = compute_nulls(i_cell=input_cell)
+    temp_null = calculated_null[max_distance]
+    
+    result = []
+    for tf in all_tfs:
+        if tf == candidate:
+            continue
+        tf1 = candidate
+        tf2 = tf
+        if tf1 > tf2:
+            tf1 = tf
+            tf2 = candidate
+        # print(tf1)
+        # print(tf2)
+
+        # contains the max line less then or equal to maxd
+        # TODO we can select all together, this will be faster
+        line = CellLineCouple.objects.filter(tf1=tf1).filter(tf2=tf2).filter(
+            distance__lte=max_distance).order_by('-distance').first()
+        # why??
+        line_null = Hepg2Null.objects.filter(tf1=tf1).filter(tf2=tf2).filter(
+            max_distance=max_distance).values().first()
+
+        if line is None:
+            continue
+
+        if line.cumulative_count_all < min_count:
+            continue
+
+        if line.cumulative_count_tss/line.cumulative_count_all < min_tss:
+            continue
+
+        # start stat testing
+        passed = []
+        scores = {}
+        for i in test_list:
+            test_name = i + "-" + str(p_value)
+            null_value = temp_null[test_name]
+            if line_null[i] is None:
+                continue
+            # TODO check correctness
+            if line_null[i] <= null_value:
+                passed.append(i)
+            scores[i] = line_null[i]
+        if min_num_true_test <= len(passed):
+            scores['name'] = tf
+            result.append(scores)
+
+    # result is list of tuples first element is name of tf
+    # and second one is passed test
+    return result
