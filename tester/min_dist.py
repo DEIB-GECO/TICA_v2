03bcbb5bee74d7d14fa0c95d0736a2cc60391574
acanakoglu <acanak@gmail.com>
2017-10-25 22:59:12 +0200

    min distance code has been corrected

--- tester/min_dist.py
+++ tester/min_dist.py
@@ -1,12 +1,13 @@
-from os import walk
-from collections import defaultdict
-from multiprocessing import Pool, Lock, Process
 import multiprocessing
+from collections import defaultdict
+from multiprocessing import Lock, Process
+from os import walk
 
-# defintions
-sameDs = True
-# if it is same then directory1 should == directory2
-directory = 'ciao/'
+from queue import Empty
+
+# definitions
+
+directory = 'cutted_joined/'
 
 max_distance = 10000
 
@@ -16,42 +17,41 @@ result_file_merged = "results/result_file_merged_par.txt"
 
 infinity = max_distance * 100 - 1
 
-# list of choromosoms that will be used by procedure
-chrs = ["chr" + str(i + 1) for i in range(22)] + ["chrx"]
+# list of chromosome
+chromosome_list = ["chr" + str(i + 1) for i in range(22)] + ["chrx"]
 
 
 # class definitions
 class Region(object):
-    __slots__ = ['tf', 'position', 'tssList']
+    __slots__ = ['tf', 'position', 'tss_set']
 
-    def __init__(self, tf, position, tssList=set()):
-        # self.chrm = chrm
+    def __init__(self, tf, position, tss_list=set()):
         self.tf = tf
         self.position = position
-        self.tssList = tssList
+        self.tss_set = tss_list
 
     def __str__(self):
-        return (self.tf + " " + str(self.position) + " " + str(self.tssList))
+        return self.tf + " " + str(self.position) + " " + str(self.tss_set)
 
     def __repr__(self):
         return str(self)
 
 
 class Distance(object):
-    __slots__ = ['distance', 'intersectTssList']
+    __slots__ = ['distance', 'intersect_tss_list']
 
-    def __init__(self, distance, intersectTssList=None):
+    def __init__(self, distance, intersect_tss_list=None):
         self.distance = distance
-        self.intersectTssList = intersectTssList
+        self.intersect_tss_list = intersect_tss_list
 
     def __str__(self):
-        return (str(self.distance) + " " + str(self.intersectTssList))
+        return str(self.distance) + " " + str(self.intersect_tss_list)
 
     def __repr__(self):
         return str(self)
 
 
-# list1 and list2(oredered) are the same chromosome list of two TFs and returns ordered list
+# list1 and list2(ordered) are the same chromosome list of two TFs and returns ordered list
 def linear_merge(list1, list2):
     # return merged list, if not any of them empty otherwise return empty
     list1 = iter(list1)
@@ -101,23 +101,23 @@ def linear_merge(list1, list2):
                         return
 
 
-#list1 and list2(oredered) are the same chromosome list of two TFs and returns the distance between the regions
+# list1 and list2(ordered) are the same chromosome list of two TFs and returns the distance between the regions
 def linear_merge_distance(list1, list2):
-    listIter = iter(linear_merge(list1, list2))
-    # set a pre region with name NO_TF at the position -infinity
-    regionPre = Region("NO_TF", -infinity)
+    list_iter = iter(linear_merge(list1, list2))
+    # set a pre region with name NO_TF at the position minus infinity
+    region_pre = Region("NO_TF", -infinity)
     # send an infinity in the beginning
     yield Distance(infinity)
 
     while True:
         try:
-            regionCurr = next(listIter)
-            if regionCurr.tf == regionPre.tf:
+            region_curr = next(list_iter)
+            if region_curr.tf == region_pre.tf:
                 yield Distance(infinity)
             else:
-                yield Distance(regionCurr.position - regionPre.position, regionCurr.tssList.intersection(regionPre.tssList))
+                yield Distance(region_curr.position - region_pre.position, region_curr.tss_set.intersection(region_pre.tss_set))
 
-            regionPre = regionCurr
+            region_pre = region_curr
         except StopIteration:
             # send an infinity at the end
             yield Distance(infinity)
@@ -128,54 +128,56 @@ lock = Lock()
 
 list_of_tf = []
 
-for (dirpath, dirnames, filenames) in walk(directory):
-    list_of_tf.extend(filenames)
+for (dir_path, dir_names, file_names) in walk(directory):
+    list_of_tf.extend(file_names)
     break
 
-print(list_of_tf)
+print("List of tfs:", list_of_tf)
+
 
 def read(tf):
-    tempTf = defaultdict(list)
+    temp_tf = defaultdict(list)
     for line in open(directory + tf):
         s = line.strip().split("\t")
-        print(s)
         tss = set() if len(s) == 2 or s[2] == "" else set([int(t) for t in s[2].split(",")])
         c = s[0].lower()
-        tempTf[c].append(Region(tf, int(s[1]), tss))
-    tempTf2 = dict()
-    for key in chrs:
-        tempTf2[key] = sorted(tempTf[key], key=lambda x: x.position)
-    return tempTf2
+        temp_tf[c].append(Region(tf, int(s[1]), tss))
+    temp_tf2 = dict()
+    # Select only in chromosome list
+    for key in chromosome_list:
+        temp_tf2[key] = sorted(temp_tf[key], key=lambda x: x.position)
+    return temp_tf2
 
 
-def getTfs(tf, tfs):
-    if not tf in tfs:
+def get_tfs(tf, tfs):
+    if tf not in tfs:
         tfs[tf] = read(tf)
     return tfs[tf]
 
 
-def calculate_distances(inp, tfs=dict()):
+def calculate_distances(inp, tfs):
     (tf1, tf2) = inp
     temp_count_all = defaultdict(int)
     temp_count_tss = defaultdict(int)
-    tf1rA = getTfs(tf1, tfs)
-    tf2rA = getTfs(tf2, tfs)
-    #out_file = open("results2/" + tf1 + "-" + tf2 + ".txt", "w")
+    tf1r_dict = get_tfs(tf1, tfs)
+    tf2r_dict = get_tfs(tf2, tfs)
+    # out_file = open("results2/" + tf1 + "-" + tf2 + ".txt", "w")
 
-    for c in chrs:
-        tf1r = tf1rA[c]
-        tf2r = tf2rA[c]
+    for c in chromosome_list:
+        tf1r = tf1r_dict[c]
+        tf2r = tf2r_dict[c]
 
         distances = linear_merge_distance(tf1r, tf2r)
         pre_distance = next(distances)
         curr_distance = next(distances)
         for postDistance in distances:
-            if curr_distance.distance < max_distance and curr_distance.distance < pre_distance.distance and curr_distance.distance <= postDistance.distance:
+            if curr_distance.distance <= max_distance and curr_distance.distance <= pre_distance.distance and curr_distance.distance <= postDistance.distance:
                 # TODO remove
-                #out_file.write("\t".join([tf1, tf2, str(curr_distance.distance), str(int(bool(curr_distance.intersectTssList)))]) + "\n")
+                # out_file.write("\t".join([tf1, tf2, str(curr_distance.distance), str(int(bool(curr_distance.intersectTssList)))]) + "\n")
+                # out_file.flush()
 
                 temp_count_all[curr_distance.distance] += 1
-                if curr_distance.intersectTssList:
+                if curr_distance.intersect_tss_list:
                     temp_count_tss[curr_distance.distance] += 1
             pre_distance = curr_distance
             curr_distance = postDistance
@@ -189,7 +191,7 @@ def calculate_distances(inp, tfs=dict()):
             cumulative_count_tss += count_tss
             out_file_merged.write("\t".join([tf1, tf2, str(dist), str(count_all), str(count_tss), str(cumulative_count_all), str(cumulative_count_tss)]) + "\n")
         out_file_merged.flush()
-    #out_file.close()
+        # out_file.close()
 
 
 out_file = open(result_file, "w")
@@ -204,16 +206,16 @@ class Consumer(multiprocessing.Process):
 
     def run(self):
         tfs = dict()
-        proc_name = self.name
-        print('%s' % (proc_name))
+        process_name = self.name
+        print('%s' % process_name)
         while True:
             next_task = self.task_queue.get()
             if next_task is None:
                 # Poison pill means shutdown
-                print('%s: Exiting' % proc_name)
+                print('%s: Exiting' % process_name)
                 self.task_queue.task_done()
                 break
-            print('%s: %s' % (proc_name, next_task))
+            print('%s: %s' % (process_name, next_task))
             answer = next_task(tfs)
             self.task_queue.task_done()
             self.result_queue.put(answer)
@@ -240,7 +242,7 @@ if __name__ == '__main__':
     results = multiprocessing.Queue()
 
     # Start consumers
-    num_consumers = min(1, int(multiprocessing.cpu_count()))
+    num_consumers = min(8, int(multiprocessing.cpu_count()))
     print('Creating %d consumers' % num_consumers)
     consumers = [Consumer(tasks, results) for i in range(num_consumers)]
     # consumers = [Consumer(tasks, results)]
@@ -265,7 +267,7 @@ if __name__ == '__main__':
             result = results.get(timeout=1)
             count += 1
             print('\t\t\tA Count: ', count, ' - Result:', result)
-        except:
+        except Empty:
             # print ('##timeout##')
             pass
     # Wait for all of the tasks to finish
