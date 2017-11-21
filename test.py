122b4cb82c7d759e49e7a85a3b311c445bea5749
Erlaad <stefano.perna@polimi.it>
2017-11-21 17:46:19 +0100

    Create update_encode function

--- test.py
+++ test.py
@@ -1,19 +1,26 @@
 import pandas as pd
 import os
 import intervaltree as it
+import numpy as np
 from tester.pipeline_steps import tfbs2tss_mapper
-
+from tester.models import CellLineTfs
 CHRS = list(map(lambda x: "chr" + str(x), range(1, 23))) + ["chrX"]
 
 
-
-
-
-def run(cell_line="hepg2_4", tfbs_files_path='media/temp/127_0_0_1_1510848168_4206731//tfbs/', tss_file_path='media/tss/gm12878.gdm'):
+def update_encode(cell_line="banane", tfbs_files_path='example/', tss_file_path='example/tss/gm12878.gdm'):
     files = list(zip(*map(lambda x: (x, os.path.join(tfbs_files_path, x)), next(os.walk(tfbs_files_path))[2])))
-
+    
+    #print(files)
+    tfs = list(map(lambda s: s.replace('.gdm', ''), files[0]))
+    filesizes = list(map(lambda f: os.stat(f).st_size, files[1]))
+    upper = np.percentile(filesizes, q=90, interpolation='higher')
+    lower = np.percentile(filesizes, q=10, interpolation='lower')
+    null_flags = list(map(lambda value: lower < value < upper, filesizes))
+    for index in range(len(tfs)):
+        new_result = CellLineTfs(cell_line=cell_line, tf=tfs[index],
+                                 use_in_null=null_flags[index])
+        new_result.save()
     temp_folder = 'media/encode/%s/' % cell_line
-
     tfbs2tss_mapper(
         files[1],
         files[0],
